492a4227ad70f8897572f3d19fc2bd4d5a87faa0
zhang <15698846866@163.com>
2019-09-08 00:16:50 +0800

    优化了代码结构

---
+++ include/Lidar_process_core.h
@@ -0,0 +1,35 @@
+#include<vector>
+#include <ros/ros.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <ctime>
+#include <chrono>
+#include <unordered_set>
+
+class LidarProcessCore
+{
+private:
+
+    ros::Publisher cloud_filtered_pub;
+    ros::Publisher obstCloud_pub;
+    ros::Publisher planeCloud_pub;
+
+    std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr,pcl::PointCloud<pcl::PointXYZI>::Ptr> 
+        SeparateClouds(pcl::PointIndices::Ptr inliers, pcl::PointCloud<pcl::PointXYZI>::Ptr cloud);
+
+    pcl::PointCloud<pcl::PointXYZI>::Ptr Cloud_filter(pcl::PointCloud<pcl::PointXYZI>::Ptr cloudinput);
+
+    std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr,pcl::PointCloud<pcl::PointXYZI>::Ptr> 
+        SegmentPlane(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, int maxIterations, float distanceThreshold);
+    
+    void Lidar_Callback(const sensor_msgs::PointCloud2ConstPtr& input);
+
+public:
+    LidarProcessCore(ros::NodeHandle & nh);
+    ~LidarProcessCore();
+};
